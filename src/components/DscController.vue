12f3c06e184abbbefad1eb39f7fcdfcd40b1be97
Valerian Backeberg <valerian@backebergs.de>
2022-04-05 17:50:41 +0200

    basic device layout

--- src/components/DscController.vue
+++ src/components/DscController.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    <img class="image-holder" src="@/assets/ic-m330.png" />
+  <div class="w-200 h-64 m-auto p-4 border-1 border-black rounded-xl">
+    <div class="h-full flex space-x-4">
+      <div class="w-1/4 flex flex-col items-center">
+        <div class="grow-1 h-full"></div>
+        <button class="ml-auto mr-2 h-20 w-20 bg-red text-white">DISTRESS</button>
+      </div>
+
+      <div class="h-full w-1/2 flex flex-col">
+        <div class="grow-1 h-full bg-orange"></div>
+        <div class="h-12 flex justify-between items-end">
+          <button class="w-12 h-8">---</button>
+          <button class="w-12 h-8">---</button>
+          <button class="w-12 h-8">---</button>
+          <button class="w-12 h-8">---</button>
+        </div>
+      </div>
+
+      <div class="h-full w-1/4 flex flex-col">
+        <div class="w-full h-8 my-2 flex">
+          <button class="mx-auto w-9">▲</button>
+        </div>
+        <div class="w-full h-8 my-2 flex justify-around">
+          <button class="w-9">◀</button>
+          <button class="w-10 text-xs tracking-tighter">ENT</button>
+          <button class="w-9">▶</button>
+        </div>
+        <div class="w-full h-8 my-2 flex justify-around">
+          <button class="w-12 text-xs tracking-tighter">MENU</button>
+          <button class="w-9">▼</button>
+          <button class="w-12 text-xs tracking-tighter">CLR</button>
+        </div>
+        <div class="w-full h-full flex items-center">
+          <button class="w-10 h-10 ml-4 mr-auto rounded-full bg-blue text-white text-xs">16/C</button>
+          <button class="w-20 h-20 rounded-full">Power</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.image-holder {
-  object-fit: scale-down;
-  width: 100%;
-  height: 100%;
+#device-frame {
+  width: 800px;
 }
 </style>

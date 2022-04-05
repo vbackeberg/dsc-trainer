d4ec210ec2835db3cdf6ff7a054f5bdb705b0ef4
Valerian Backeberg <valerian@backebergs.de>
2022-04-05 17:49:36 +0200

    unocss with tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Unocss from "unocss/vite";
+import presetWind from "@unocss/preset-wind";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Unocss({
+      presets: [presetWind()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
